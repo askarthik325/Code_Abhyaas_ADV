23909398eef3cc27da1b2a91dc48f4dff643f0a4
askarthik325 <askarthik325@gmail.com>
2023-08-25 00:13:02 +0530

    rambola

---
+++ sorted.cpp
@@ -0,0 +1,35 @@
+// program to check weather the array is sorted or not...
+#include<bits/stdc++.h>
+using namespace std;
+void sorted(int arr[],int n){
+    bool asc,dsc = true; // asc = ascending , dsc = descending
+    for(int i=0;i<n-1;i++){
+        if(arr[i+1]>=arr[i]){
+            asc=false;
+            break;
+        }
+        if(arr[i+1]<=arr[i]){
+            dsc=false;
+            break;
+        }
+    }
+    if(asc||dsc){
+        cout << "Array is Sorted..!";
+    }
+    else{
+        cout << "Array is not Sorted..!";
+    }
+
+}
+int main(){
+    int n;
+    cout<< "Enter the size of array:";
+    cin>> n;
+    int arr[n];
+    cout<<"Enter elements to the array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sorted(arr,n);
+    return 0;
+}
