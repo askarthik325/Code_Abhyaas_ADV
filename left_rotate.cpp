e614850d17c21783ac709fb41296f28cbdc97e8c
askarthik325 <askarthik325@gmail.com>
2023-08-25 10:53:09 +0530

    left rotating an array

--- left_rotate.cpp
+++ left_rotate.cpp
@@ -1,1 +1,27 @@
-#
+// Program for left roatating an elment in an array.
+#include<bits/stdc++.h>
+using namespace std;
+int leftrotate(int arr[],int n){
+    int temp=arr[0];
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+
+}
+int main(){
+    int n;
+    cout<< "Enter the size of array:";
+    cin>> n;
+    int arr[n];
+    cout<<"Enter elements to the array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    leftrotate(arr,n);
+    cout << "The array after left rotate is :";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
